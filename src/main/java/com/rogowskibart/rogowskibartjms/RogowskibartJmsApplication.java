54de597b771fa000990d7e168a0998e4e94ead93
Bartosz Rogowski <rogowskibart@gmail.com>
2021-06-23 17:35:10 +0200

    Initial commit

---
+++ src/main/java/com/rogowskibart/rogowskibartjms/RogowskibartJmsApplication.java
@@ -0,0 +1,13 @@
+package com.rogowskibart.rogowskibartjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RogowskibartJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RogowskibartJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rogowskibart/rogowskibartjms/RogowskibartJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogowskibart.rogowskibartjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogowskibartJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
